84cc7e378c05074ef57c692f96bc0a559f4c937c
David <davidnezri1990@gmail.com>
2023-09-04 14:33:13 +0300

    changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-// import express from "express";
+
 import express from "express"
 import cors from "cors";
 import catchAll from "./3-middleware/catch-all";
@@ -8,7 +8,7 @@ import config from "./2-utils/config";
 import expressFileUpload from "express-fileupload";
 import authController from "./6-controllers/auth-controller";
 import sanitize from "./3-middleware/sanitize"
-import expressRateLimit from "express-rate-limit"
+//import expressRateLimit from "express-rate-limit"
 
 // Create server object
 const server = express();
@@ -21,15 +21,15 @@ server.use(express.json());
 
 server.use('/static',express.static('src/1-assets'))
 
-server.use(
-     "/api/",
-  expressRateLimit({
-    windowMs: 500,
-    max: 50,
-    message:
-      "You have exceeded the allowed amount of times for browsing the site.",
-  })
-);
+// server.use(
+//      "/api/",
+//   expressRateLimit({
+//     windowMs: 500,
+//     max: 50,
+//     message:
+//       "You have exceeded the allowed amount of times for browsing the site.",
+//   })
+// );
 
 // Sanitize  
 server.use(sanitize)
